f31b3014e686395428f21864a26cd5b7eee7d0e8
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-08-13 14:32:10 +0300

    first commit

---
+++ src/main/java/nabil/hibernateadvancedmapping/HibernateAdvancedMappingApplication.java
@@ -0,0 +1,13 @@
+package nabil.hibernateadvancedmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAdvancedMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateAdvancedMappingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nabil/hibernateadvancedmapping/HibernateAdvancedMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package nabil.hibernateadvancedmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAdvancedMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
